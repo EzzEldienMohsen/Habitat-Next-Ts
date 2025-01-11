f7fdcbbf294307aafd5c3df169eb635d4e0224af
Ezz dghedy <ezzmohsend@gmail.com>
2025-01-12 00:00:43 +0200

    deployment error handling

--- app/(client)/address/[id]/page.tsx
+++ app/(client)/address/[id]/page.tsx
@@ -1,8 +1,15 @@
 import EditAddressComponent from '@/components/addressComponents/EditAddressComponent';
 import Separator from '@/components/mainPageComponents/Separator';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import React from 'react';
 
-const page: React.FC<{ params: { id: string } }> = ({ params }) => {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+const Page: React.FC<PageProps> = ({ params }) => {
   const { id } = params;
   return (
     <>
@@ -12,4 +19,15 @@ const page: React.FC<{ params: { id: string } }> = ({ params }) => {
   );
 };
 
-export default page;
+// export const getServerSideProps: GetServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   const { id } = context.params as { id: string };
+//   return {
+//     props: {
+//       params: { id },
+//     },
+//   };
+// };
+
+export default Page;
