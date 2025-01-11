e37bca5b77cebe1b171b3f8389a7e62fcc87c24e
Ezz dghedy <ezzmohsend@gmail.com>
2025-01-12 01:32:07 +0200

    deployment error handling

--- app/(client)/address/[id]/page.tsx
+++ app/(client)/address/[id]/page.tsx
@@ -3,22 +3,13 @@ import EditAddressComponent from '@/components/addressComponents/EditAddressComp
 import Separator from '@/components/mainPageComponents/Separator';
 import { Params } from 'next/dist/server/request/params';
 
-interface PageProps {
-  params: Params;
-}
-
-const Page: React.FC<PageProps> = ({ params }) => {
-  const id = params.id;
-
-  if (!id) {
-    console.error('No ID found in params');
-    return (
-      <div className="w-full flex justify-center items-center my-8">
-        <span className="text-red-500">Error: No ID found in params</span>
-      </div>
-    );
+const Page: React.FC<{ params: Params }> = async ({ params }) => {
+  let id;
+  try {
+    id = await params.id;
+  } catch (error) {
+    console.log(error);
   }
-
   return (
     <>
       <EditAddressComponent id={id} />
